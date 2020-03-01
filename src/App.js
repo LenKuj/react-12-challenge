b6097d20edbe2458bb88256df8b0929f8502aa0d
Lena Kujath <l.kujath@gmx.de>
2020-03-01 21:42:40 +0100

    fixed the tab selector

--- src/App.js
+++ src/App.js
@@ -8,26 +8,39 @@ const Contact = () => <p>Feel free to contact us!</p>;
 const Error = () => <p>Something went <strong>wrong</strong>!</p>;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-  }
 
-  getTabContent() {
+
+      state = {
+
+      activeId: 'home'
+
+    };
+ 
+
+    handleChangeTab = (event) => {
+
+      const buttonId = event.target.id;
+      this.setState({ activeId: buttonId });
+    }
+  
+
+  getTabContent = () => {
     switch(this.state.activeId) {
       case 'home':
         return <Home />;
+        case 'about':
+          return <About />;
+      case 'contact':
+          return <Contact />;
       default:
         return <Error />;
     }
   }
 
-  render() {
+  render () {
     return (
       <div className="App">
-        <TabSelector />
+          <TabSelector activeId={this.state.activeId} handleChangeTab={this.handleChangeTab}/>
         <div className="App-content">
           {
             this.getTabContent()
@@ -37,5 +50,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
